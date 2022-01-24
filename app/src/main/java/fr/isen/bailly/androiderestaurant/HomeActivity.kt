dca03d4e048a1f746e1f52d5d94e20642ae59fe2
MatheoBailly <66616107+MatheoBailly@users.noreply.github.com>
2022-01-24 18:39:38 +0100

    fin de journée commit

--- app/src/main/java/fr/isen/bailly/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/bailly/androiderestaurant/HomeActivity.kt
@@ -2,8 +2,9 @@ package fr.isen.bailly.androiderestaurant
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.Toast
+//import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
@@ -18,18 +19,25 @@ class HomeActivity : AppCompatActivity() {
         val btnDessert = findViewById<Button>(R.id.btnDessert)
 
         btnStarter.setOnClickListener{
-            val toast = Toast.makeText(this@HomeActivity, "Vous avez sélectionné l'entrée", Toast.LENGTH_SHORT)
-            toast.show()
+            //Toast.makeText(this@HomeActivity, "Vous avez sélectionné l'entrée", Toast.LENGTH_SHORT).show()
+            changeActivity(getString(R.string.home_starters))
         }
 
         btnMainDish.setOnClickListener{
-            val toast = Toast.makeText(this@HomeActivity, "Vous avez sélectionné le plat", Toast.LENGTH_SHORT)
-            toast.show()
+            //val toast = Toast.makeText(this@HomeActivity, "Vous avez sélectionné le plat", Toast.LENGTH_SHORT).show()
+            changeActivity(getString(R.string.home_main_dishes))
         }
 
         btnDessert.setOnClickListener{
-            val toast = Toast.makeText(this@HomeActivity, "Vous avez sélectionné le dessert", Toast.LENGTH_SHORT)
-            toast.show()
+            //val toast = Toast.makeText(this@HomeActivity, "Vous avez sélectionné le dessert", Toast.LENGTH_SHORT).show()
+            changeActivity(getString(R.string.home_desserts))
         }
+    }
+
+    private fun changeActivity( category: String ) {
+        val changePage = Intent(this@HomeActivity, MainDishActivity::class.java)
+        changePage.putExtra("category_type",category)
+        Log.i("INFO", "End of HomeActivity")
+        startActivity(changePage)
     }
 }
--- app/src/main/java/fr/isen/bailly/androiderestaurant/MainDishActivity.kt
+++ app/src/main/java/fr/isen/bailly/androiderestaurant/MainDishActivity.kt
@@ -1,6 +1,7 @@
 package fr.isen.bailly.androiderestaurant
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainDishActivity : AppCompatActivity() {
@@ -8,5 +9,8 @@ class MainDishActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_dish)
+
+        val category = intent.getStringExtra("category_type")
+        findViewById<TextView>(R.id.mainDishTitle).text = category
     }
 }
